60d57020ff4173384221c63cfdece39633df99f0
Michael Kim <miketkim@umich.edu>
2019-10-15 14:05:20 -0500

    one node linked list implementation

--- LinkedList.py
+++ LinkedList.py
@@ -2,4 +2,17 @@ class Node:
     def __init__(self, value):
         self.value = value 
         self.next = None
-    def (self)
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+    llist = SinglyLinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    print(llist.head.value)
+    print(llist.head.next == None)
